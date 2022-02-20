34dc3e5d7e64f237700530d9b85cd146ec05478a
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-02-20 09:35:07 +0530

    pushZeros to end

--- src/PushZerosToEnd.java
+++ src/PushZerosToEnd.java
@@ -1,2 +1,29 @@
 public class PushZerosToEnd {
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 2, 3, 4, 0, 0, 0, 4};
+        pushZerostoEnd(arr);
+        printArray(arr);
+    }
+
+    public static void pushZerostoEnd(int[] arr) {
+        int k = 0; //sorted array Index , it will update only when nonzero element found ..
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+                k++;
+            }
+        }
+    }
+
+        public static void printArray ( int[] arr){
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+
+
 }
+
