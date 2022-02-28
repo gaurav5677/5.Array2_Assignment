a1b8cca459507be4d9dc0810b046b471275d10e8
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-02-28 14:43:01 +0530

    rotate the array for n times

--- src/PushZerosToEnd.java
+++ src/PushZerosToEnd.java
@@ -8,10 +8,12 @@ public class PushZerosToEnd {
     public static void pushZerostoEnd(int[] arr) {
         int k = 0; //sorted array Index , it will update only when nonzero element found ..
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                int temp = arr[i];
-                arr[i] = arr[k];
-                arr[k] = temp;
+            if (arr[i] != 0) {// here we are checking whether the next num is zero or not
+                //if it is non zero then we well replace it with the temp variable and
+                // we will increment i but not the k ,  k will  increment when  non zero element is found
+                int temp  = arr[i];
+                arr[i] = arr[k]; //arr{i}= arr{nonzero}
+                arr[k] = temp;//arr{nonzero}=temp;
                 k++;
             }
         }
